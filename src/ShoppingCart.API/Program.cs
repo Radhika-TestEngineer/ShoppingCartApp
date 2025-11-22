e62722c48592537ece2bf62d990fddd9dd0bea89
Radhika-TestEngineer <radhika.sivva@gmail.com>
2025-11-22 11:57:12 +0530

    Update Program.cs17

--- src/ShoppingCart.API/Program.cs
+++ src/ShoppingCart.API/Program.cs
@@ -94,6 +94,9 @@ app.MapControllers();
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
 
+app.MapGet("/", () => "ShoppingCart API is running!");
+
+
 app.Run();
 
 
