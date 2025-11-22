c986bae499e9ddc9c37150a1c9672d06c2a32972
Radhika-TestEngineer <radhika.sivva@gmail.com>
2025-11-22 12:23:56 +0530

    Update Program.cs

--- src/ShoppingCart.API/Program.cs
+++ src/ShoppingCart.API/Program.cs
@@ -70,7 +70,7 @@ app.MapGet("/", () => "ShoppingCart API is running on Render!");
 
 app.MapControllers();
 
-app.Run();*/
+app.Run();
 
 using ShoppingCart.Core.Services;
 using ShoppingCart.Infrastructure.Services;
@@ -96,6 +96,42 @@ app.Urls.Add($"http://0.0.0.0:{port}");
 
 app.MapGet("/", () => "ShoppingCart API is running!");
 
+
+app.Run();*/
+
+
+using ShoppingCart.Core.Services;
+using ShoppingCart.Infrastructure.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// THIS IS THE MAGIC LINE FOR RENDER
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+builder.Services.AddControllers();
+builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger works in production too
+app.UseSwagger();
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShoppingCart API V1");
+    c.RoutePrefix = "swagger"; // go to /swagger
+});
+
+// This fixes the 404 on root
+app.MapGet("/", () => Results.Content(@"
+    <h1>ShoppingCart API is running on Render!</h1>
+    <p><a href=""/swagger"">Go to Swagger UI</a></p>
+", "text/html"));
+
+// Your real API endpoints
+app.MapControllers();
 
 app.Run();
 
