52d49af7a256196be12e06005918ad515f56dce5
Radhika-TestEngineer <radhika.sivva@gmail.com>
2025-11-22 11:44:29 +0530

    Update Program.cs15

--- src/ShoppingCart.API/Program.cs
+++ src/ShoppingCart.API/Program.cs
@@ -42,7 +42,7 @@ app.MapGet("/", () => "ShoppingCart API is running on Render!");
 
 app.MapControllers();
 
-app.Run();*/
+app.Run();
 
 
 using ShoppingCart.Core.Services;
@@ -70,6 +70,31 @@ app.MapGet("/", () => "ShoppingCart API is running on Render!");
 
 app.MapControllers();
 
+app.Run();*/
+
+using ShoppingCart.Core.Services;
+using ShoppingCart.Infrastructure.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger always enabled
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+// Bind to Render PORT
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
+
 app.Run();
+
 
 
